58a3dc4f1493cb45f016c2833a2efcafe304a2d5
YihaoWang0127 <yihaowang0127@gmail.com>
2020-10-05 18:21:57 -0700

    The initial version

---
+++ cse360assignment02/AddingMachine.java
@@ -0,0 +1,45 @@
+package cse360assignment02;
+
+public class AddingMachine {
+	 private int total;
+	  
+	 /**
+	   * This method is the constructor of AddingMachine.
+	   */
+	  public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  
+	  /**
+	   * This method is used to get the sum of multiple objects
+	   */
+	  public int getTotal () {
+	    return 0;
+	  }
+	  
+	  /**
+	   * This method is used to add a number to the total value.
+	   */
+	  public void add (int value) {
+	  }
+	  
+	  /**
+	   * This method is used to subtract the number from the total value.
+	   */
+	  public void subtract (int value) {
+	  }
+
+	  /**
+	   * This method is used to print out the outcome in string format
+	   */
+	  public String toString () {
+	    return "";
+	  }
+	  
+	  /**
+	   * This method is used to clear and reset the total value.
+	   */
+	  public void clear() {
+	  }
+
+}
