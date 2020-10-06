843076d7281f2838c3ad04ce47b101412939dcf4
YihaoWang0127 <yihaowang0127@gmail.com>
2020-10-05 18:46:52 -0700

    The second version

--- cse360assignment02/AddingMachine.java
+++ cse360assignment02/AddingMachine.java
@@ -1,45 +1,51 @@
 package cse360assignment02;
 
 public class AddingMachine {
-	 private int total;
-	  
-	 /**
-	   * This method is the constructor of AddingMachine.
-	   */
-	  public AddingMachine () {
-	    total = 0;  // not needed - included for clarity
-	  }
-	  
-	  /**
-	   * This method is used to get the sum of multiple objects
-	   */
-	  public int getTotal () {
-	    return 0;
-	  }
-	  
-	  /**
-	   * This method is used to add a number to the total value.
-	   */
-	  public void add (int value) {
-	  }
-	  
-	  /**
-	   * This method is used to subtract the number from the total value.
-	   */
-	  public void subtract (int value) {
-	  }
+	private int total;
+	private String str = "0";
 
-	  /**
-	   * This method is used to print out the outcome in string format
-	   */
-	  public String toString () {
-	    return "";
-	  }
-	  
-	  /**
-	   * This method is used to clear and reset the total value.
-	   */
-	  public void clear() {
-	  }
+	/**
+	 * This method is the initial constructor of AddingMachine.
+	 */
+	public AddingMachine() {
+		total = 0;  // not needed - included for clarity
+	}
+
+	/**
+	 * This method is used to get the sum.
+	 */
+	public int getTotal() {
+		return total;
+	}
+
+	/**
+	 * This method is used to add a number to total.
+	 */
+	public void add(int value) {
+		total += value;
+		str = str +  " + " + value;
+	}
+
+	/**
+	 * This method is used to subtract the number from the total.
+	 */
+	public void subtract(int value) {
+		total -= value;
+		str = str + " - " + value;
+	}
+
+	/**
+	 * This method is used to print out the outcome in string format
+	 */
+	public String toString() {
+		return str;
+	}
+
+	/**
+	 * This method is used to clear and reset the total value.
+	 */
+	public void clear() {
+		total = 0;
+	}
 
 }
